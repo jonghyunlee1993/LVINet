c499cd36dc0dd117326ab4bb03993cd5a4c8e15f
Jonghyun Lee <green198800@hanyang.ac.kr>
2024-01-09 16:42:36 +0900

    Initial commit

---
+++ YOLOX/exps/default/yolov3_LVI.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+# Copyright (c) Megvii, Inc. and its affiliates.
+
+import os
+
+import torch.nn as nn
+
+from yolox.exp import Exp as MyExp
+
+
+class Exp(MyExp):
+    def __init__(self):
+        super(Exp, self).__init__()
+        self.depth = 1.0
+        self.width = 1.0
+        self.exp_name = "yolov3_LVI"
+	
+        self.data_dir = "datasets/LVI_coco"
+        self.train_ann = "train_labels.json"
+        self.val_ann = "valid_labels.json"
+        self.test_ann = "test_labels.json"
+
+        self.num_classes = 1
+
+        self.max_epoch = 100
+        self.data_num_workers = 12
+        self.eval_interval = 1
+
+    def get_model(self, sublinear=False):
+        def init_yolo(M):
+            for m in M.modules():
+                if isinstance(m, nn.BatchNorm2d):
+                    m.eps = 1e-3
+                    m.momentum = 0.03
+        if "model" not in self.__dict__:
+            from yolox.models import YOLOX, YOLOFPN, YOLOXHead
+            backbone = YOLOFPN()
+            head = YOLOXHead(self.num_classes, self.width, in_channels=[128, 256, 512], act="lrelu")
+            self.model = YOLOX(backbone, head)
+        self.model.apply(init_yolo)
+        self.model.head.initialize_biases(1e-2)
+
+        return self.model
---
+++ YOLOX/exps/default/yolox_s_LVI.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+# Copyright (c) Megvii, Inc. and its affiliates.
+import os
+
+from yolox.exp import Exp as MyExp
+
+
+class Exp(MyExp):
+    def __init__(self):
+        super(Exp, self).__init__()
+        self.depth = 0.33
+        self.width = 0.50
+        self.exp_name = "yolox_s_LVI"
+
+        # Define yourself dataset path
+        self.data_dir = "datasets/LVI_coco"
+        self.train_ann = "train_labels.json"
+        self.val_ann = "valid_labels.json"
+        self.test_ann = "test_labels.json"
+
+        self.num_classes = 1
+
+        self.max_epoch = 100
+        self.data_num_workers = 12
+        self.eval_interval = 1
